255d87112cf130160430bb75a7660a2fbf0a39df
surajmandal738 <suraj.mandal738@gmail.com>
2019-04-06 13:55:05 +0530

    commit -v 1.1

--- src/main.py
+++ src/main.py
@@ -7,14 +7,34 @@ data = json.load(open('./data.json'))
 dictionary_available_words = data.keys()
 
 
+def print_pretty_output(word: str) -> None:
+    """
+    print the output in human readable form
+    :param word:
+    :return:
+    """
+    for (index, definition) in enumerate(data[word]):
+        print(f'{index + 1}. {definition}')
+
+
 # improving the performance of the application
-def find_similar_word(word):
+def find_similar_word(word: str) -> list or None:
+    """
+    returns the word, which is most similar with the argument else returns None
+    :param word:
+    :return:
+    """
     filtered_list = list(filter(lambda x: x.startswith(word[0]), dictionary_available_words))
     closest_match = ''.join(get_close_matches(word, filtered_list, n=1))
     return closest_match if closest_match else None
 
 
-def translate(word):
+def translate(word: str) -> None:
+    """
+    prints the definition of the word and implements the login of the above two function
+    :param word:
+    :return:
+    """
     if word not in dictionary_available_words:
         # checking for similar words
         similar_word = find_similar_word(word)
@@ -23,12 +43,12 @@ def translate(word):
         else:
             choice = input(f'Did you mean {similar_word}? (Y / N) : ')
             if choice.lower() == 'y':
-                print(data[similar_word])
+                print_pretty_output(similar_word)
             else:
                 print("The word is not present in the dictionary")
 
     else:
-        print(data[word])
+        print_pretty_output(word)
 
 
 if __name__ == '__main__':
